758cd2f53254c314978ae73a0f52370c5f4ca998
huanghuang <huanghuang@douban.com>
2012-09-03 14:23:35 +0800

    你说你都没提供api，搞毛啊

--- douban_client/api/music.py
+++ douban_client/api/music.py
@@ -6,5 +6,37 @@ class Music(DoubanApiBase):
 
     def __repr__(self):
         return '<DoubanAPI Music>'
+    """
+    def get(self, id):
+        return self._get('/v2/music/%s/'%id)
 
+    def search(self, q=None, tag=None, start=DEFAULT_START, count=DEFAULT_COUNT):
+        query = ''
+        if q:
+            query = q
+        elif tag:
+            query = tag
+        return self._get('/v2/music/search', q=query, start=start, count=count)
+
+    def reviews(self, id, start=DEFAULT_START, count=DEFAULT_COUNT):
+        return self._get('/v2/music/%s/review'%id, start=start,count=count)
+
+    def top_tags(self, id, start=DEFAULT_START, count=DEFAULT_COUNT):
+        return self._get('/v2/music/%s/tags'%id, start=start, count=count)
+
+    def review(self, id):
+        return self._get('/v2/music/review/%s'%id)
+
+    def create_review(self, music, title, content, rating=None):
+        return self._post('/v2/music/reviews', music=music, title=title, content=content, rating=rating)
+
+    def update_review(self, id, title, content, rating=None):
+        return self._put('/v2/music/review/%s'%id, title=title, content=content, rating=rating)
+
+    def delete_review(self, id):
+        return self._delete('/v2/music/review/%s'%id)
+
+    def all_tags(self, id, start=DEFAULT_START, count=DEFAULT_COUNT):
+        return self._get('/v2/music/people_tags/%s'%id, start=start, count=count)
+    """
 
--- tests/test_api_music.py
+++ tests/test_api_music.py
@@ -6,6 +6,15 @@ class TestApiMusic(DoubanClientTestBase):
     def setUp(self):
         super(TestApiMusic, self).setUp()
         self.user_id = '40774605'
+        self.music_id = '11524982'
+        self.title = "this is a good album"
+        self.content = "wanting wanting wanting"
+
+    """
+    def test_get_music(self):
+        ret = self.client.music.get(self.music_id)
+        self.assertEqual(ret['id'], self.music_id)
+    """
 
 if __name__ == '__main__':
     main()
