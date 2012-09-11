a1bdb23eb8b9f5fd3e2ccd0c332e703da2e9ad10
liluo <i@liluo.org>
2012-09-11 18:04:06 +0800

    补充日记、豆邮接口中的 start, count 参数

--- douban_client/api/discussion.py
+++ douban_client/api/discussion.py
@@ -16,8 +16,8 @@ class Discussion(DoubanApiBase):
     def new(self, target, target_id, title, content):
         return self._post('/v2/%s/%s/discussions'%(target, target_id), title=title, content=content)
 
-    def list(self, target, target_id):
-        return self._get('/v2/%s/%s/discussions'%(target, target_id))
+    def list(self, target, target_id, start=DEFAULT_START, count=DEFAULT_COUNT):
+        return self._get('/v2/%s/%s/discussions'%(target, target_id), start=start, count=count)
 
     def update(self, id, title, content):
         return self._put('/v2/discussion/%s'%id, title=title, content=content)
